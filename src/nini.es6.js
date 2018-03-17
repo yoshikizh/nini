58aed1117153ace674f1f53454760b21c0f73440
yoshikizh <177365340@qq.com>
2018-03-17 17:57:32 +0800

    scale

--- src/nini.es6.js
+++ src/nini.es6.js
@@ -38,18 +38,19 @@ class Graphics {
 
         this.ctx.globalAlpha = (1 / 255) * sprite.opacity
 
-        let _x = sprite.x - sprite.ox * bitmap.width
-        let _y = sprite.y - sprite.oy * bitmap.height
+        let _x = sprite.x - sprite.ox * (bitmap.width * sprite.scale)
+        let _y = sprite.y - sprite.oy * (bitmap.height * sprite.scale)
 
-        let _ctx_ox = _x + sprite.ox * bitmap.width
-        let _ctx_oy = _y + sprite.oy * bitmap.height
+        let _ctx_ox = _x + sprite.ox * bitmap.width * sprite.scale
+        let _ctx_oy = _y + sprite.oy * bitmap.height * sprite.scale
 
         this.ctx.save()
         this.ctx.translate(_ctx_ox,_ctx_oy)
         this.ctx.rotate(sprite.angle*Math.PI/180);
         this.ctx.translate(-_ctx_ox,-_ctx_oy)
-        this.ctx.drawImage( bitmap.img ,0 ,0 , bitmap.width, bitmap.height, _x, _y, bitmap.width, bitmap.height )
-
+        // this.ctx.scale(sprite.scale,sprite.scale);
+        this.ctx.drawImage( bitmap.img ,0 ,0 , bitmap.width, bitmap.height, _x, _y, bitmap.width* sprite.scale, bitmap.height* sprite.scale )
+        // this.ctx.scale(1,1);
         this.ctx.rotate(0)
         this.ctx.globalAlpha = 1
         this.ctx.restore()
