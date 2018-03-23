c4818846c2e80d0166cb689c600b425380e32458
yoshikizh <177365340@qq.com>
2018-03-23 22:38:44 +0800

    table && tilemap

---
+++ src/table.js
@@ -0,0 +1,7 @@
+class Table {
+  constructor(xsize, ysize, zsize) {
+    this.xsize = xsize 
+    this.ysize = ysize
+    this.zsize = zsize
+  }
+}
---
+++ src/tilemap.js
@@ -0,0 +1,18 @@
+class Tilemap {
+  
+  constructor(img, viewport){
+    this.bitmaps = []
+    this.map_data = new Table()
+    this.viewport = viewport
+    this.ox = 0
+    this.oy = 0
+  }
+
+  update(){
+
+  }
+
+  dispose(){
+
+  }
+}
